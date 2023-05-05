8e75cedf5dd60a751c2e80fe4b0a9400f3f46950
lks <endygamessk@gmail.com>
2023-05-05 16:59:56 +0200

    changed mapbox to leaflet map

--- scripts/script.js
+++ scripts/script.js
@@ -13,13 +13,8 @@ document.querySelectorAll('#global-visits tr').forEach(tr => tr.onclick = e => {
 })
 
 var map = L.map('map').setView([61.505, -10.09], 3);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZmFzaGlvbmtpbGxhIiwiYSI6ImNsMmtwb2l0MjBmZHUzam84cTVobzVlbTIifQ.zL-QCXz6inlps2PqFLTXtw'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 fetch('map.php').then(res => res.json()).then(data => {
